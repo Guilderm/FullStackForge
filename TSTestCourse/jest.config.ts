8f91e751b0ecfc402f6305103df21375635a9758
Guilder W. Milliner <guilderw@gmail.com>
2024-09-02 16:19:00 -0600

    Section 3 complite

--- TSTestCourse/jest.config.ts
+++ TSTestCourse/jest.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from '@jest/types'
+import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+};
 
 export default config;
