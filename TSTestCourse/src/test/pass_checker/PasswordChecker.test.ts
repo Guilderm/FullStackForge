329085ea11da324af5eb94ae832f690d92f0e519
Guilder W. Milliner <guilderw@gmail.com>
2024-09-02 16:45:22 -0600

    updated wording for the test suit

--- TSTestCourse/src/test/pass_checker/PasswordChecker.test.ts
+++ TSTestCourse/src/test/pass_checker/PasswordChecker.test.ts
@@ -3,59 +3,63 @@ import {
   PasswordErrors,
 } from "../../app/pass_checker/PasswordChecker";
 
-describe("PasswordChecker test suite", () => {
+describe("PasswordChecker Test Suite", () => {
   let sut: PasswordChecker;
 
   beforeEach(() => {
     sut = new PasswordChecker();
   });
 
-  it("Password with less than 8 chars is invalid", () => {
-    const actual = sut.checkPassword("1234567");
-    expect(actual.valid).toBe(false);
-    expect(actual.reasons).toContain(PasswordErrors.SHORT);
+  describe("checkPassword method should", () => {
+    it("invalidate a password with less than 8 characters", () => {
+      const actual = sut.checkPassword("1234567");
+      expect(actual.valid).toBe(false);
+      expect(actual.reasons).toContain(PasswordErrors.SHORT);
+    });
+
+    it("validate a password with 8 or more characters", () => {
+      const actual = sut.checkPassword("12345678");
+      expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
+    });
+
+    it("invalidate a password with no uppercase letters", () => {
+      const actual = sut.checkPassword("abcd");
+      expect(actual.valid).toBe(false);
+      expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+    });
+
+    it("validate a password with at least one uppercase letter", () => {
+      const actual = sut.checkPassword("abcD");
+      expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
+    });
+
+    it("invalidate a password with no lowercase letters", () => {
+      const actual = sut.checkPassword("ABCD");
+      expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
+    });
+
+    it("validate a password with at least one lowercase letter", () => {
+      const actual = sut.checkPassword("ABCDa");
+      expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
+    });
+
+    it("validate a complex password with uppercase, lowercase, and numbers", () => {
+      const actual = sut.checkPassword("1234abcD");
+      expect(actual.reasons).toHaveLength(0);
+      expect(actual.valid).toBe(true);
+    });
   });
 
-  it("Password with more than 8 chars is ok", () => {
-    const actual = sut.checkPassword("12345678");
-    expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
-  });
+  describe("checkAdminPassword method should", () => {
+    it("invalidate an admin password with no numbers", () => {
+      const actual = sut.checkAdminPassword("abcdABCD");
+      expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
+      expect(actual.valid).toBe(false);
+    });
 
-  it("Password with no upper case letter is invalid", () => {
-    const actual = sut.checkPassword("abcd");
-    expect(actual.valid).toBe(false);
-    expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
-  });
-
-  it("Password with upper case letter is valid", () => {
-    const actual = sut.checkPassword("abcD");
-    expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
-  });
-
-  it("Password with no lower case letter is invalid", () => {
-    const actual = sut.checkPassword("ABCD");
-    expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
-  });
-
-  it("Password with lower case letter is valid", () => {
-    const actual = sut.checkPassword("ABCDa");
-    expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
-  });
-
-  it("Complex password is valid", () => {
-    const actual = sut.checkPassword("1234abcD");
-    expect(actual.reasons).toHaveLength(0);
-    expect(actual.valid).toBe(true);
-  });
-
-  it("Admin password with no number is invalid", () => {
-    const actual = sut.checkAdminPassword("abcdABCD");
-    expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
-    expect(actual.valid).toBe(false);
-  });
-
-  it("Admin password with number is valid", () => {
-    const actual = sut.checkAdminPassword("abcdABCD7");
-    expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
+    it("validate an admin password with at least one number", () => {
+      const actual = sut.checkAdminPassword("abcdABCD7");
+      expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
+    });
   });
 });
